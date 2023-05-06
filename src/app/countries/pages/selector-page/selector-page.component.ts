0f01d97e413b33306ff60d65e76f3a8697a12d56
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-05-05 22:11:49 -0500

    Tercer selector anidado de fronteras

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interface';
-import { switchMap, tap } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'countries-selector-page',
@@ -13,9 +13,10 @@ export class SelectorPageComponent implements OnInit {
   public myForm: FormGroup = this.fb.group({
     region: ['', [Validators.required]],
     country: ['', [Validators.required]],
-    borders: ['', [Validators.required]],
+    border: ['', [Validators.required]],
   });
   public countriesByRegion: SmallCountry[] = [];
+  public borders: string[] = [];
 
   constructor(
     private fb: FormBuilder,
@@ -24,6 +25,7 @@ export class SelectorPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.onRegionChange();
+    this.onCountryChange();
   }
 
   get regions(): Region[] {
@@ -35,10 +37,24 @@ export class SelectorPageComponent implements OnInit {
       .get('region')!
       .valueChanges.pipe(
         tap(() => this.myForm.get('country')!.setValue('')),
+        tap(() => (this.borders = [])),
         switchMap((region) =>
           this.countriesService.getCountriesByRegion(region)
         )
       )
       .subscribe((countries) => (this.countriesByRegion = countries));
+  };
+
+  onCountryChange = (): void => {
+    this.myForm
+      .get('country')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('border')!.setValue('')),
+        filter((value: string) => value.length > 0),
+        switchMap((alphacode) =>
+          this.countriesService.getCountryByAlphaCode(alphacode)
+        )
+      )
+      .subscribe((country) => (this.borders = country.borders));
   };
 }
